383c620451bebdc37b0a8dad0ad5764ca444fa5e
Tonći Vatavuk <tonci@sistemi.hr>
2018-01-08 11:19:32 +0100

    Migrate original inpage code

---
+++ src/toolbar/toolbarManager.{}.js
@@ -0,0 +1,12 @@
+﻿// the toolbar manager is an internal helper
+// taking care of toolbars, buttons etc.
+(function () {
+    /**
+     * Toolbar manager for the whole page - basically a set of APIs
+     */
+    $2sxc._toolbarManager = {
+
+        // internal constants
+        cDisableAttrName: "data-disable-toolbar"
+    };
+})();
