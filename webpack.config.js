20cc395190bdf7f71d893d0128e807c41fe17685
Tonći Vatavuk <tonci@sistemi.hr>
2018-01-08 16:54:20 +0100

    wip 2018-01-08

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+﻿"use strict";
+
+var glob = require('glob');
+
+module.exports = {
+
+    // get all js files for boundle
+    entry: glob.sync('./src/**/*.js'),
+
+    output: {
+        path: __dirname + '/dist',
+        filename: 'inpage.js'
+    },
+
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+            { test: /\.tsx?$/, loader: "awesome-typescript-loader" }
+        ]
+    }
+
+};
