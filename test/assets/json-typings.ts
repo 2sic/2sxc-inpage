060a819278dcde244e6dd400ac2bd637627604ca
Tonći Vatavuk <tonci@sistemi.hr>
2018-01-18 08:36:41 +0100

    update

--- test/assets/json-typings.ts
+++ test/assets/json-typings.ts
@@ -1,4 +1,8 @@
-﻿// https://hackernoon.com/import-json-into-typescript-8d465beded79
+﻿/**
+ * Wildcard module declarations *.json
+ * allow non-JavaScript content to be imported
+ * https://hackernoon.com/import-json-into-typescript-8d465beded79
+ */
 declare module "*.json" {
     const value: any;
     export default value;
