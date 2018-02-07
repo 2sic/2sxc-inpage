87f9bd988240fd03a0faa1c0a52f6ddb47a719e5
Tonći Vatavuk <tonci@sistemi.hr>
2018-02-07 08:24:26 +0100

    commandCreate more typed

--- src/commands/command-create.ts
+++ src/commands/command-create.ts
@@ -1,10 +1,7 @@
-﻿import { translate } from '../translate/2sxc.translate';
-import { $2sxc as twoSxc } from '../x-bootstrap/module-bootstrapper';
+﻿import { $2sxc as twoSxc } from '../x-bootstrap/module-bootstrapper';
 import { Settings } from './settings';
 import { Command } from './command';
-import { Params } from './params';
 import { DataEditContext } from '../data-edit-context/data-edit-context';
-import { NgDialogParams } from '../manage/ng-dialog-params';
 
 /**
  * assemble an object which will store the configuration and execute it
@@ -14,15 +11,15 @@ import { NgDialogParams } from '../manage/ng-dialog-params';
  */
 export function commandCreate(sxc: SxcInstanceWithInternals, editContext: DataEditContext, specialSettings: Settings): Command {
 
-  let settings: Settings = Object.assign(sxc.manage._instanceConfig, specialSettings) as Settings; // merge button with general toolbar-settings
+  const settings: Settings = Object.assign(sxc.manage._instanceConfig, specialSettings) as Settings; // merge button with general toolbar-settings
 
-  let ngDialogUrl: string = editContext.Environment.SxcRootUrl +
+  const ngDialogUrl: string = editContext.Environment.SxcRootUrl +
     'desktopmodules/tosic_sexycontent/dist/dnn/ui.html?sxcver=' +
     editContext.Environment.SxcVersion;
 
-  let isDebug: string = twoSxc.urlParams.get('debug') ? '&debug=true' : '';
+  const isDebug: string = twoSxc.urlParams.get('debug') ? '&debug=true' : '';
 
-  let cmd = new Command(sxc, settings, ngDialogUrl, isDebug);
+  const cmd = new Command(sxc, settings, ngDialogUrl, isDebug);
   
   return cmd;
 }
