1d7124c42d1d5b9a92967cf0f6424a3cc575899e
Tonći Vatavuk <tonci@sistemi.hr>
2018-02-11 23:29:00 +0100

    refactor QuickE from interface to class

--- src/quick-edit/$quickE.{}.ts
+++ src/quick-edit/$quickE.{}.ts
@@ -1,49 +1,49 @@
-﻿import { QuickE } from './quick-e';
-import { Selectors } from './selectors';
+﻿import { Conf } from './conf';
+import { Coords } from './coords';
 
-// the quick-edit object
-// the quick-insert object
-export let $quickE = window.$quickE = {
-  body: $('body'),
-  win: $(window),
-  main: $("<div class='sc-content-block-menu sc-content-block-quick-insert sc-i18n'></div>"),
-  template: "<a class='sc-content-block-menu-addcontent sc-invisible' data-type='Default' data-i18n='[titleTemplate]QuickInsertMenu.AddBlockContent'>x</a>"
+/**
+ * the quick-edit object
+ * the quick-insert object
+ */
+class QuickE {
+  body: any = $('body');
+  win: any = $(window);
+  main = $("<div class='sc-content-block-menu sc-content-block-quick-insert sc-i18n'></div>");
+  template: string = '<a class=\'sc-content-block-menu-addcontent sc-invisible\' data-type=\'Default\' data-i18n=\'[titleTemplate]QuickInsertMenu.AddBlockContent\'>x</a>'
   + "<a class='sc-content-block-menu-addapp sc-invisible' data-type='' data-i18n='[titleTemplate]QuickInsertMenu.AddBlockApp'>x</a>"
   + btn('select', 'ok', 'Select', true)
-  + btn('paste', 'paste', 'Paste', true, true),
-  selected: $("<div class='sc-content-block-menu sc-content-block-selected-menu sc-i18n'></div>")
+  + btn('paste', 'paste', 'Paste', true, true);
+  selected: any = $("<div class='sc-content-block-menu sc-content-block-selected-menu sc-i18n'></div>")
     .append(
     btn('delete', 'trash-empty', 'Delete'),
     btn('sendToPane', 'export', 'Move', null, null, 'sc-cb-mod-only'),
     "<div id='paneList'></div>",
-    ),
-  contentBlocks: null,
-  cachedPanes: null,
-  modules: null,
-  nearestCb: null,
-  nearestMod: null,
-  modManage: null, // will be populated later in the module section
-} as QuickE;
+  );
+  // will be populated later in the module section
+  contentBlocks: any = null;
+  cachedPanes: any = null;
+  modules: any = null;
+  nearestCb: any = null;
+  nearestMod: any = null;
+  modManage: any = null;
+  // add stuff which depends on other values to create
+  cbActions: any = $(this.template);
+  modActions: any = $(this.template.replace(/QuickInsertMenu.AddBlock/g, 'QuickInsertMenu.AddModule'))
+    .attr('data-context', 'module')
+    .addClass('sc-content-block-menu-module');
+  //
+  config: Conf;
+  bodyOffset: Coords;
+}
 
-
-
-
-
-
-
+export let $quickE = window.$quickE = new QuickE();
 
 function btn(action: string, icon: string, i18N: string, invisible?: boolean, unavailable?: boolean, classes?: string): string {
-  return "<a class='sc-content-block-menu-btn sc-cb-action icon-sxc-" + icon + ' '
+  return '<a class=\'sc-content-block-menu-btn sc-cb-action icon-sxc-' + icon + ' '
     + (invisible ? ' sc-invisible ' : '')
     + (unavailable ? ' sc-unavailable ' : '')
-    + classes + "' data-action='" + action + "' data-i18n='[title]QuickInsertMenu." + i18N + "'></a>";
-};
-
-// add stuff which depends on other values to create
-$quickE.cbActions = $($quickE.template);
-$quickE.modActions = $($quickE.template.replace(/QuickInsertMenu.AddBlock/g, 'QuickInsertMenu.AddModule'))
-  .attr('data-context', 'module')
-  .addClass('sc-content-block-menu-module');
+    + classes + '\' data-action=\'' + action + "' data-i18n='[title]QuickInsertMenu." + i18N + "'></a>";
+}
 
 /**
  * build the toolbar (hidden, but ready to show)
@@ -54,4 +54,3 @@ export function prepareToolbarInDom(): void {
   $quickE.main.append($quickE.cbActions)
     .append($quickE.modActions);
 }
-
