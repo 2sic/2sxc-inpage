f695d6e5484f865fb6463cccb12fa95fa5dd27e1
Tonći Vatavuk <tonci@sistemi.hr>
2018-03-22 14:33:59 +0100

    toolbar with one button "new" and settings { "hover": "left" }

--- test/toolbar.ts
+++ test/toolbar.ts
@@ -76,33 +76,53 @@ describe('Toolbar test suite', function () {
     expect(generatedHtml).toBe(expectedHtml);
   });
 
-  //it('8 mock standard toolbar with settings { "hover": "left", "autoAddMore": "start" } to contain button more as first button in toolbar', function () {
-  //  const toolbarData: any = {};
-  //  const toolbarSettings: ToolbarSettings = { "hover": "left", "autoAddMore": "start" } as ToolbarSettings;
+  it('8 mock toolbar with one button "new" and settings { "hover": "left" } to contain `sc-tb-hover-left`', function () {
+    const toolbarData: any = [{ "action": "new", "contentType": "Dummy" }];
+    const toolbarSettings: ToolbarSettings = { "hover": "left" } as ToolbarSettings;
+    //toolbarSettings.hover = 'left';
+    const cnt = getContextFromEditContext(editContext);
+    cnt.toolbar = ExpandToolbarConfig(cnt, toolbarData, toolbarSettings);
+    const generatedHtml = renderToolbar(cnt);
+    const containHtml = `sc-tb-hover-left`;
+    expect(generatedHtml).toContain(containHtml);
+  });
+
+  it('9 mock toolbar with one button "new" and settings { "hover": "left" }', function () {
+    const toolbarData: any = [{ "action": "new", "contentType": "Dummy" }];
+    const toolbarSettings: ToolbarSettings = { "hover": "left" } as ToolbarSettings;
+    //toolbarSettings.hover = 'left';
+    const cnt = getContextFromEditContext(editContext);
+    cnt.toolbar = ExpandToolbarConfig(cnt, toolbarData, toolbarSettings);
+    const generatedHtml = renderToolbar(cnt);
+    const expectedHtml = `<ul class="sc-menu group-0 sc-tb-hover-left sc-tb-show-hover" onclick="var e = arguments[0] || window.event; e.stopPropagation();" group-count="1"><li><a class="sc-new group-0" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;new&quot;,&quot;contentType&quot;:&quot;Dummy&quot;}, event);" data-i18n="[title]Toolbar.New"><div><i class="icon-sxc-plus" aria-hidden="true"></i></div></a></li></ul>`;
+    expect(generatedHtml).toBe(expectedHtml);
+  });
+
+  //it('10 mock toolbar one button "new" and settings { "hover": "left" } to contain `sc-tb-hover-left`', function () {
+  //  const toolbarData: any = [{ "action": "new", "contentType": "Dummy" }];
+  //  const toolbarSettings: ToolbarSettings = { "hover": "left" } as ToolbarSettings;
   //  //toolbarSettings.hover = 'left';
-  //  //toolbarSettings.autoAddMore = 'end';
   //  const cnt = getContextFromEditContext(editContext);
   //  cnt.toolbar = ExpandToolbarConfig(cnt, toolbarData, toolbarSettings);
   //  const generatedHtml = renderToolbar(cnt);
   //  console.log(
   //    'stv: html',
   //    generatedHtml);
-  //  const containHtml = `<ul class="sc-menu group-0 sc-tb-hover-left sc-tb-show-hover" onclick="var e = arguments[0] || window.event; e.stopPropagation();" group-count="4"><li><a class="sc-more group-0" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;more&quot;}, event);" data-i18n="[title]Toolbar.MoreActions"><div><i class="icon-sxc-options btn-mode" aria-hidden="true"></i></div></a></li>`;
+  //  const containHtml = `sc-tb-hover-left`;
   //  expect(generatedHtml).toContain(containHtml);
   //});
 
-  //it('9 mock standard toolbar with settings "hover": "left" ', function () {
-  //  const toolbarData: any = {};
-  //  const toolbarSettings: ToolbarSettings = { "hover": "left", "autoAddMore": "start" } as ToolbarSettings;
+  //it('11 mock toolbar one button "new" and settings "hover": "left" ', function () {
+  //  const toolbarData: any = [{ "action": "new", "contentType": "Dummy" }];
+  //  const toolbarSettings: ToolbarSettings = { "hover": "left" } as ToolbarSettings;
   //  //toolbarSettings.hover = 'left';
-  //  //toolbarSettings.autoAddMore = 'end';
   //  const cnt = getContextFromEditContext(editContext);
   //  cnt.toolbar = ExpandToolbarConfig(cnt, toolbarData, toolbarSettings);
   //  const generatedHtml = renderToolbar(cnt);
   //  console.log(
   //    'stv: html',
   //    generatedHtml);
-  //  const expectedHtml = `<ul class="sc-menu group-0 sc-tb-hover-left sc-tb-show-hover" onclick="var e = arguments[0] || window.event; e.stopPropagation();" group-count="4"><li><a class="sc-more group-0" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;more&quot;}, event);" data-i18n="[title]Toolbar.MoreActions"><div><i class="icon-sxc-options btn-mode" aria-hidden="true"></i></div></a></li><li><a class="sc-layout group-0" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;layout&quot;}, event);" data-i18n="[title]Toolbar.ChangeLayout"><div><i class="icon-sxc-glasses" aria-hidden="true"></i></div></a></li><li><a class="sc-more group-1" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;more&quot;}, event);" data-i18n="[title]Toolbar.MoreActions"><div><i class="icon-sxc-options btn-mode" aria-hidden="true"></i></div></a></li><li><a class="sc-item-history group-1" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;item-history&quot;}, event);" data-i18n="[title]Toolbar.ItemHistory"><div><i class="icon-sxc-clock" aria-hidden="true"></i></div></a></li><li><a class="sc-more group-2 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;more&quot;}, event);" data-i18n="[title]Toolbar.MoreActions"><div><i class="icon-sxc-options btn-mode" aria-hidden="true"></i></div></a></li><li><a class="sc-template-develop group-2 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;template-develop&quot;}, event);" data-i18n="[title]Toolbar.Develop"><div><i class="icon-sxc-code" aria-hidden="true"></i></div></a></li><li><a class="sc-template-settings group-2 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;template-settings&quot;}, event);" data-i18n="[title]Toolbar.TemplateSettings"><div><i class="icon-sxc-sliders" aria-hidden="true"></i></div></a></li><li><a class="sc-contentitems group-2 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;contentitems&quot;}, event);" data-i18n="[title]Toolbar.ContentItems"><div><i class="icon-sxc-table" aria-hidden="true"></i></div></a></li><li><a class="sc-template-query group-2 disabled group-pro empty" onclick="" data-i18n="[title]Toolbar.QueryEditDisabled"><div><i class="icon-sxc-filter" aria-hidden="true"></i></div></a></li><li><a class="sc-contenttype group-2 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;contenttype&quot;}, event);" data-i18n="[title]Toolbar.ContentType"><div><i class="icon-sxc-fields" aria-hidden="true"></i></div></a></li><li><a class="sc-more group-3 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;more&quot;}, event);" data-i18n="[title]Toolbar.MoreActions"><div><i class="icon-sxc-options btn-mode" aria-hidden="true"></i></div></a></li><li><a class="sc-app group-3 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;app&quot;}, event);" data-i18n="[title]Toolbar.App"><div><i class="icon-sxc-settings" aria-hidden="true"></i></div></a></li><li><a class="sc-app-settings group-3 disabled group-pro empty" onclick="" data-i18n="[title]Toolbar.AppSettingsDisabled"><div><i class="icon-sxc-sliders" aria-hidden="true"></i></div></a></li><li><a class="sc-app-resources group-3 disabled group-pro empty" onclick="" data-i18n="[title]Toolbar.AppResourcesDisabled"><div><i class="icon-sxc-language" aria-hidden="true"></i></div></a></li><li><a class="sc-zone group-3 group-pro" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;zone&quot;}, event);" data-i18n="[title]Toolbar.Zone"><div><i class="icon-sxc-manage" aria-hidden="true"></i></div></a></li></ul>`;
+  //  const expectedHtml = `<ul class="sc-menu group-0 sc-tb-hover-left sc-tb-show-hover" onclick="var e = arguments[0] || window.event; e.stopPropagation();" group-count="1"><li><a class="sc-new group-0" onclick="$2sxc(2506, 2506).manage.run2($2sxc.context(this), {&quot;action&quot;:&quot;new&quot;,&quot;contentType&quot;:&quot;Dummy&quot;}, event);" data-i18n="[title]Toolbar.New"><div><i class="icon-sxc-plus" aria-hidden="true"></i></div></a></li></ul>`;
   //  expect(generatedHtml).toBe(expectedHtml);
   //});
 
