22b857922f94774f1b3e6306add760f51643c8cb
ijungleboy <daniel.mettler@2sic.com>
2020-01-14 18:50:23 +0100

    minor: update installer to use $2sxc insead of $.ajax

--- src/2sxc-extensions/2sxc.system.ts
+++ src/2sxc-extensions/2sxc.system.ts
@@ -10,13 +10,10 @@ if (window.$2sxc && !window.$2sxc.system) {
 // upgrade command - started when an error contains a link to start this
 function finishUpgrade(domElement: HTMLElement): void {
   const mc = window.$2sxc(domElement);
-  $.ajax({
-    type: 'get',
-    url: mc.resolveServiceUrl('view/module/finishinstallation'),
-    beforeSend: $.ServicesFramework(mc.id).setModuleHeaders,
-  }).success(() => {
-    alert('Upgrade ok, restarting the CMS and reloading...');
-    location.reload();
-  });
+  mc.webApi.get('view/module/finishinstallation')
+    .success(() => {
+      alert('Upgrade ok, restarting the CMS and reloading...');
+      location.reload();
+    });
   alert('starting upgrade. This could take a few minutes. You\'ll see an \'ok\' when it\'s done. Please wait...');
 }
